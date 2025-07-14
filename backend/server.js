568025d0f96b88e559daa77520c67e0463f87711
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-14 16:56:07 +0530

    backend files added

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send(
+        `<h1>sajalgit </h1>`
+    )
+})
+
+app.listen(5001, () => {
+  console.log("Server is running on PORT 5001");
+});
