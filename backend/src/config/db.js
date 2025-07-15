3ee9cd66dd8f6f0815892bdd87e338d65234208b
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-15 23:51:20 +0530

    mongDB connection added

---
+++ backend/src/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://uniquesajal6:Sajal26@cluster0.zxd9uyx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("Error in connecting MongoDB", err);
+  }
+};
