2a58d444706c76d292d3e15c8ba9b69a6e57f255
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-15 23:39:04 +0530

    modification

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,3 +7,6 @@ app.use("/api/notes", notesRoutes);
 app.listen(5001, () => {
   console.log("Server is running on PORT 5001");
 });
+
+
+// mongodb+srv://uniquesajal6:Sajal26@cluster0.zxd9uyx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
