a1400bfeb5a0d526a7b657b5aa9eaa19e8797391
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-15 22:56:25 +0530

    modification

--- backend/routes/notes.route.js
+++ backend/routes/notes.route.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { createNote, getAllNotes } from "../controllers/notes.controller";
+import {
+  createNote,
+  deleteNote,
+  getAllNotes,
+  updateNote,
+} from "../controllers/notes.controller.js";
 
 const router = express.Router();
 
 router.get("/", getAllNotes);
 router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
 
 export default router;
