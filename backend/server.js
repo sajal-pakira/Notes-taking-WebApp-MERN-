7ccf7b09f63bde78ccd578609d68fb29e619ff1f
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-15 20:46:04 +0530

    another script added

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,9 @@ import express from "express";
 
 const app = express();
 
-app.get("/api/notes",(req,res)=>{
-    res.send(
-        `<h1>sajalgit </h1>`
-    )
-})
+app.get("/api/notes", (req, res) => {
+  res.send(`<h1>sajal</h1>`);
+});
 
 app.listen(5001, () => {
   console.log("Server is running on PORT 5001");
