1610333e17ea6630fa9b9969d91b0d892bcc28a4
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-15 21:14:36 +0530

    modification

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,6 @@ import notesRoutes from "./routes/notes.routes";
 const app = express();
 
 app.use("/api/notes", notesRoutes);
-app.get("/api/notes", (req, res) => {
-  res.send(`<h1>sajal</h1>`);
-});
 
 app.listen(5001, () => {
   console.log("Server is running on PORT 5001");
