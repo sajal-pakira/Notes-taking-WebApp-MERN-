22bc3f931f3f02046455677f2a5dc5043150c474
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-16 18:16:47 +0530

    modification

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,19 @@
 import express from "express";
 import notesRoutes from "./routes/notes.route.js";
 import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
+// console.log(process.env.MONGO_URI);
+const port = process.env.PORT || 5001;
 
 connectDB();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-  console.log("Server is running on PORT 5001");
+app.listen(port, () => {
+  console.log("Server is running on PORT : ", port);
 });
 
 //
