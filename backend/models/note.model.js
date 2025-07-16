3cfcdda3acba0565d4fe7d5c7517873e14a94ae3
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-16 19:53:50 +0530

    title added

--- backend/models/note.model.js
+++ backend/models/note.model.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
-const noteSchema = new mongoose.Schema({}, { timeseries: true });
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+  },
+  { timeseries: true }
+);
 
 const Note = mongoose.model("Note", noteSchema);
 
