7c0591616678b6b1c2ee73c3daf31f74bbc70c4a
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-19 23:29:31 +0530

    daisyui@4.12.24 added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
       <Toaster />
-      <button onClick={() => toast.success("Congrats")}>click me</button>
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreateNote />} />
