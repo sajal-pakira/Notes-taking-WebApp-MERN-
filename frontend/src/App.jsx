a6de7184cd34205e5b533a5304cb3491a7bc6e77
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-19 22:17:20 +0530

    NoteDetailPage route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage.jsx";
 import CreateNote from "./pages/CreatNote.jsx";
+import NoteDetailPage from "./pages/NoteDetailPage.jsx";
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreateNote />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
       </Routes>
     </div>
   );
