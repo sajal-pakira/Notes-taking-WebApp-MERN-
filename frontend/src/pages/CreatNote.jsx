2c7cbe8e98d72cf9284caebf2003b0d54b2dd200
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-19 22:13:08 +0530

    CreateNote page added

---
+++ frontend/src/pages/CreatNote.jsx
@@ -0,0 +1,7 @@
+const CreatNote = () => {
+  return (
+    <div>CreatNote</div>
+  )
+}
+
+export default CreatNote
--- frontend/src/pages/CreatPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CreatPage = () => {
-  return (
-    <div>CreatPage</div>
-  )
-}
-
-export default CreatPage
