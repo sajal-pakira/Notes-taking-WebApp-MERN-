f1dc0c7380207bafcf28e90b057e0819e266b853
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-19 22:15:39 +0530

    CreateNote route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage.jsx";
+import CreateNote from "./pages/CreatNote.jsx";
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreateNote />} />
       </Routes>
     </div>
   );
