e5f9cd763f152689fdfa99e2eb6e9a5a54f1f542
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-19 23:05:35 +0530

    react hot toast added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,12 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage.jsx";
 import CreateNote from "./pages/CreatNote.jsx";
 import NoteDetailPage from "./pages/NoteDetailPage.jsx";
+import toast, { Toaster } from "react-hot-toast";
 const App = () => {
   return (
     <div>
+      <Toaster />
+      <button onClick={() => toast.success("Congrats")}>click me</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreateNote />} />
