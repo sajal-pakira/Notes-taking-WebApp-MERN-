dc2e46a55806ae7993f1031ebbfb995de5d3ca65
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-20 12:17:57 +0530

    RateLimitedUI added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import NavBar from "../components/NavBar";
+import RateLimitedUI from "../components/RateLimitedUI";
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
   return (
     <div className="min-h-screen">
       <NavBar />
+      {isRateLimited && <RateLimitedUI />}
     </div>
   );
 };
