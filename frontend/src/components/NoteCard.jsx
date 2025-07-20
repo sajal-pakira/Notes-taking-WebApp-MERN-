4f852427d4093c6a196436f7ad64b37ab3c2434c
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-20 23:27:52 +0530

    modification

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -5,7 +5,11 @@ const NoteCard = ({ note }) => {
     <Link
       to={`/note/${note._id}`}
       className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9D]"
-    ></Link>
+    >
+      <div className="card-body">
+        <h3 className="card-title text-base-content">{note.title}</h3>
+      </div>
+    </Link>
   );
 };
 
