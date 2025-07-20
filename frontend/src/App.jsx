285440736116e0650e8af9bda0010884b3ae03e6
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-20 09:49:36 +0530

    daisy ui themes are added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,12 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage.jsx";
 import CreateNote from "./pages/CreatNote.jsx";
 import NoteDetailPage from "./pages/NoteDetailPage.jsx";
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 const App = () => {
   return (
-    <div>
+    <div data-theme="night">
       <Toaster />
-      <button className="btn btn-primary">click me</button>
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreateNote />} />
