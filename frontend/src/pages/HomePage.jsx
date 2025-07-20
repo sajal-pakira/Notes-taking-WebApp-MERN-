362e460dc686d3d6c0476f64ccdbf55b7f226ee0
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-20 12:41:03 +0530

    useState & useEffect added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import RateLimitedUI from "../components/RateLimitedUI";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {}, []);
+
   return (
     <div className="min-h-screen">
       <NavBar />
