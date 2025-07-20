740e71120ad29783f83b9f1589b70c8fc93a59b5
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-20 10:03:10 +0530

    modification

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 const HomePage = () => {
   return (
     <header className="bg-base-300 border-b border-base-content/10">
-      <div className="mx-auto max-w-6xl p-4 "></div>
+      <div className="mx-auto max-w-6xl p-4 ">
+        <div className="flex items-center justify-between"></div>
+      </div>
     </header>
   );
 };
