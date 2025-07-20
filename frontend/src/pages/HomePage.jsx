b4a20f7c020bf2087e19617cfdc754193b2fe5cb
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-20 10:06:26 +0530

    h1 added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,11 @@ const HomePage = () => {
   return (
     <header className="bg-base-300 border-b border-base-content/10">
       <div className="mx-auto max-w-6xl p-4 ">
-        <div className="flex items-center justify-between"></div>
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold font-mono tracking-tighter">
+            ThinkBoard
+          </h1>
+        </div>
       </div>
     </header>
   );
