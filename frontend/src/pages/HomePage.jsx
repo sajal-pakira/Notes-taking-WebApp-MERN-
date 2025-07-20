e0e420d41b352e02350acf719d28f5381258635f
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-20 10:01:29 +0530

    modification

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,9 @@
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <header className="bg-base-300 border-b border-base-content/10">
+      <div className="mx-auto max-w-6xl p-4 "></div>
+    </header>
+  );
 };
 
 export default HomePage;
