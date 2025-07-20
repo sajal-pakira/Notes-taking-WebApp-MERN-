a6997c9f7fe5cca36fceeb7ce1fa12948697c4d5
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-20 11:29:14 +0530

    modification

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router";
+import { Plus } from "lucide-react";
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <header className="bg-base-300 border-b border-base-content/10">
+      <div className="mx-auto max-w-6xl p-4 ">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">
+            ThinkBoard
+          </h1>
+          <div className="flex items-center gap-4">
+            <Link to={"/create"} className="btn btn-primary">
+              <Plus className="size-5" />
+              <span>Create new note</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default NavBar
+export default NavBar;
