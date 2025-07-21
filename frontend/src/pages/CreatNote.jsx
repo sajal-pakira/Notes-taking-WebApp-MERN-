3bcc5d9fa039feb4c014bc95c0d6c8809f077558
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-21 13:28:10 +0530

    modification

--- frontend/src/pages/CreatNote.jsx
+++ frontend/src/pages/CreatNote.jsx
@@ -12,14 +12,17 @@ const CreateNote = () => {
   return (
     <div className="min-h-screen bg-base-200">
       <div className="container mx-auto px-4 py-8">
-        <div className="max-w-2xl mx-auto">
+        <div className="max-w-3xl mx-auto">
           <Link to={"/"} className="btn btn-ghost mb-6">
             <ArrowLeft className="size-5" />
             Back to Notes
           </Link>
           <div className="card bg-base-100">
             <div className="card-body">
-              <h2 className="card-title text-2xl mb-4"></h2>
+              <h2 className="card-title text-2xl mb-4">Create New Note</h2>
+              <form onSubmit={handleSubmit}>
+                <div></div>
+              </form>
             </div>
           </div>
         </div>
