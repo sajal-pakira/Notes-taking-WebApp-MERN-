78521b80ea2cde9f33b91f85b069e6f7eefe32cd
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-21 13:09:37 +0530

    modification

--- frontend/src/pages/CreatNote.jsx
+++ frontend/src/pages/CreatNote.jsx
@@ -4,7 +4,11 @@ const CreateNote = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
-  return <div>CreateNote</div>;
+  return (
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8"></div>
+    </div>
+  );
 };
 
 export default CreateNote;
