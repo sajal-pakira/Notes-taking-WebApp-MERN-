ce71a97c549ca723bd3e2ac8d99c7c9295cae3e0
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-21 12:11:11 +0530

    modification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,9 @@ import NoteDetailPage from "./pages/NoteDetailPage.jsx";
 import { Toaster } from "react-hot-toast";
 const App = () => {
   return (
-    <div data-theme="night">
+    <div  className="relative w-full h-full">
       <Toaster />
-
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreateNote />} />
--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -6,7 +6,7 @@ const NoteCard = ({ note }) => {
   return (
     <Link
       to={`/note/${note._id}`}
-      className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00ccff]"
+      className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00b7ff]"
     >
       <div className="card-body">
         <h3 className="card-title text-base-content">{note.title}</h3>
