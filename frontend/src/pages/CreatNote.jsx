8879448870c2000c521e6c1f059021d097711b1e
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-21 13:05:02 +0530

    one more useState

--- frontend/src/pages/CreatNote.jsx
+++ frontend/src/pages/CreatNote.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
+
 const CreateNote = () => {
   const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   return <div>CreateNote</div>;
 };
 
