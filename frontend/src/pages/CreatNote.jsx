466d058fdec7abd082cf6a2fbf143051563d4ed6
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-21 13:19:51 +0530

    back arrow added

--- frontend/src/pages/CreatNote.jsx
+++ frontend/src/pages/CreatNote.jsx
@@ -1,3 +1,4 @@
+import { ArrowLeft } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router";
 
@@ -12,7 +13,10 @@ const CreateNote = () => {
     <div className="min-h-screen bg-base-200">
       <div className="container mx-auto px-4 py-8">
         <div className="max-w-2xl mx-auto">
-          <Link to={"/"} className="btn btn-ghost mb-6"></Link>
+          <Link to={"/"} className="btn btn-ghost mb-6">
+            <ArrowLeft />
+            back to notes
+          </Link>
         </div>
       </div>
     </div>
