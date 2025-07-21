f2a640935f73a94a907b86fa200b9680948a357f
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-21 13:13:36 +0530

    modification

--- frontend/src/pages/CreatNote.jsx
+++ frontend/src/pages/CreatNote.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
+import { Link } from "react-router";
 
 const CreateNote = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+
   return (
     <div className="min-h-screen bg-base-200">
-      <div className="container mx-auto px-4 py-8"></div>
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <Link to={"/"} className="btn btn-ghost mb-6"></Link>
+        </div>
+      </div>
     </div>
   );
 };
