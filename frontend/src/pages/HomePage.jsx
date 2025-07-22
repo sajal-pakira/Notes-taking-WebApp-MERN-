57d7728e9c799b444e004a315174a6e78c39d34b
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-22 21:28:58 +0530

    NoteNotFound component added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -48,6 +48,8 @@ const HomePage = () => {
             Notes Loading...
           </div>
         )}
+        {notes.length===0 && !isRateLimited && <NoteNotFound />}
+
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
