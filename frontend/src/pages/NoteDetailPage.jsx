2c6bd529ffb068f3da1fd1325ad051b0cef34ca5
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-22 23:26:48 +0530

    useNavigate added

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const NoteDetailPage = () => {
   const [loading, setLoading] = useState(true);
   const [saving, setSaving] = useState(false);
   const [notes, setNotes] = useState(null);
+
+  const navigate = useNavigate();
 
   return <div>NoteDetailPage</div>;
 };
