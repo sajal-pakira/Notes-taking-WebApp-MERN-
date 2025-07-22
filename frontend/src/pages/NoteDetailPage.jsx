e99337dbb72689e0b6348692a7e705a63f9e39a6
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-22 23:24:13 +0530

    useState added

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,7 +1,11 @@
-const NoteDetailPage = () => {
-  return (
-    <div>NoteDetailPage</div>
-  )
-}
+import { useState } from "react";
 
-export default NoteDetailPage
+const NoteDetailPage = () => {
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
+  const [notes, setNotes] = useState(null);
+
+  return <div>NoteDetailPage</div>;
+};
+
+export default NoteDetailPage;
