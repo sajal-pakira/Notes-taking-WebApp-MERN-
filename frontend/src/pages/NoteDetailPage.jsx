40509e53e23ad94d21f361f3cb1eaa1d7d6dece1
Sajal Pakira <uniquesajal6@gmail.com>
2025-07-23 19:51:54 +0530

    modification

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -43,6 +43,7 @@ const NoteDetailPage = () => {
   return (
     <div className="min-h-screen bg-base-200 ">
       <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto"></div>
         <div className="flex items-center justify-between mb-6">
           <Link to={"/"} className="btn btn-ghost">
             <ArrowLeft className="h-5 w-5" />
